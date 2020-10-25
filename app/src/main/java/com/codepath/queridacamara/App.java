d9a91fc3d7b8a6f0c268353f101aac0675fa4f90
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-10-25 13:24:50 -0600

    Define Data Object and Query

--- app/src/main/java/com/codepath/queridacamara/App.java
+++ app/src/main/java/com/codepath/queridacamara/App.java
@@ -3,12 +3,16 @@ package com.codepath.queridacamara;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
--- app/src/main/java/com/codepath/queridacamara/MainActivity.java
+++ app/src/main/java/com/codepath/queridacamara/MainActivity.java
@@ -3,13 +3,23 @@ package com.codepath.queridacamara;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    TextView tvTitleNewPost;
     EditText etDescription;
     Button btnTakePicture;
     ImageView ivPostPicture;
@@ -20,9 +30,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etDescription.findViewById(R.id.etDescription);
-        btnTakePicture.findViewById(R.id.btnTakePicture);
-        ivPostPicture.findViewById(R.id.ivPostPicture);
-        btnPostPicture.findViewById(R.id.btnTakePicture);
+        tvTitleNewPost = findViewById(R.id.tvTitleNewPost);
+        etDescription = findViewById(R.id.etDescription);
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        ivPostPicture = findViewById(R.id.ivPostPicture);
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
