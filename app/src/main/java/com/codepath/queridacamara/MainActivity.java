3adf82adbb5be35c7f20a770694458901fd99692
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-10-29 14:10:56 -0600

    created home (post) fragment

--- app/src/main/java/com/codepath/queridacamara/MainActivity.java
+++ app/src/main/java/com/codepath/queridacamara/MainActivity.java
@@ -3,12 +3,15 @@ package com.codepath.queridacamara;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.codepath.queridacamara.fragments.ComposeFragment;
+import com.codepath.queridacamara.fragments.PostFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.parse.ParseUser;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fabLogOut;
     ParseUser currentUser;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -43,19 +47,23 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 Fragment fragment;
                 switch (menuItem.getItemId()) {
-                    case R.id.action_home:
-                        break;
                     case R.id.action_compose:
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
+                        fragment = new ComposeFragment();
                         break;
+                    // R.id.action_home
                     default:
+                        fragment = new PostFragment();
                         break;
                 }
-                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     private void logUserOut() {
---
+++ app/src/main/java/com/codepath/queridacamara/fragments/PostFragment.java
@@ -0,0 +1,29 @@
+package com.codepath.queridacamara.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.queridacamara.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PostFragment extends Fragment {
+
+    public PostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+}
